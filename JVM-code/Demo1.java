d623f9ad1ebbbede09577765883d3fc9aa9be2a8
lirui <634178463@qq.com>
2018-11-02 13:24:22 +0800

    编译并修改运行时JVM参数

---
+++ JVM-code/Demo1.java
@@ -0,0 +1,8 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("输入的参数" + (i + 1) + "：" + args[i]);
+        }
+        System.out.println("-Xmx" + Runtime.getRuntime().maxMemory() / 1000 / 1000 + "M");
+    }
+}
